adea2f27b029f346d83bc589fa38a03677caba88
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-19 09:17:45 +0200

    refactor(server): change jobs api directory

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/user.entity';
-import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GlobalHttpModule } from './global-http.module';
 import { JobsModule } from './jobs/jobs.module';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
