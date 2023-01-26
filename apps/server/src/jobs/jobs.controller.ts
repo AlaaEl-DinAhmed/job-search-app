31694f043ff3cd6d64cec2786bcb08a2212880c5
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-26 07:56:04 +0200

    feat(server): add authGuard to jobs api

--- apps/server/src/jobs/jobs.controller.ts
+++ apps/server/src/jobs/jobs.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../guards/auth.guard';
 import { JobQueryDto } from './jobs.dto';
 import { JobsService } from './jobs.service';
 
@@ -7,6 +8,7 @@ export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getJobs(@Query() query: JobQueryDto) {
     return this.jobsService.getJobs(query);
   }
