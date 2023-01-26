e19e9aa06198af88e9194c91515e72b7932256e2
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-26 17:00:40 +0200

    refactor(job-search-app): remove styles.css

--- apps/job-search-app/pages/_app.tsx
+++ apps/job-search-app/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
